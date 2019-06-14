18a5a13944ae8e672634a8118dc113b3131e4b3d
ItelSunday <iteldomingotech@gmail.com>
2019-06-14 12:04:27 -0700

    updated algo-answer, set idea for SortingRobot

--- Short-Answer/Algorithms_Answers.md
+++ Short-Answer/Algorithms_Answers.md
@@ -1,4 +1,5 @@
 Add your answers to the Algorithms exercises here.
+## Exercise I
 
 ```
 a)  a = 0
@@ -7,7 +8,7 @@ a)  a = 0
 ```
 - The loop is 0(n) as it doesn't modifies n, every loop just increases and 0(n) terms dominates 0(1).
 
-
+```
 b)  sum = 0 // 0(1)
     for i in range(n): // 0(n) * 0(n^3) = 0(^4)
       i += 1
@@ -17,16 +18,16 @@ b)  sum = 0 // 0(1)
           k += 1
           for l in range(k + 1, 10 + k): //0(n) * 0(n) = 0(^n)
             sum += 1 // 0(1)
-
+```
 - 0(1) + 0(n^4) = 0(n^4) The nested for loops dominates
 
-
+```
 c)  def bunnyEars(bunnies):
       if bunnies == 0: //stops then counts back the total of bunney ears.
         return 0
 
       return 2 + bunnyEars(bunnies-1) //At the reduction, the function calls itself with a smaller number each time
-
+```
 - 0(2) + 0(n) = 0(2n) = 0(n)
 Recusive function 
 It loops through each bunny. 
--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -97,12 +97,35 @@ class SortingRobot:
         Sort the robot's list.
         """
         # Fill this out
-        pass
+        while self.light
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
+    
+    # Main comman for Sorting Robot
+        #self._item = None       # The item the robot is holding
+        #self._position = 0      # The list position the robot is at
+        #self._light = "OFF"     # The state of the robot's light
+        #self._time = 0          # A time counter (stretch)
+    
+    # Library of Robots movement/command
+        #self.can_move_left   :   move left if at the start of the list
+        #self.can_move_right  :   move right if at the end of the list
+        
+        #self.move_left       :   returns true if robot move left. Otherwise, it stays in place and returns False (increments 1)
+        #self.move_right      :   returns true if robot move right. Otherwise, it stays in place and returns False (increments 1)
+        
+        #self.swap_item       :   swaps current held item with the list in front
+        #self.compare_item    :   compares current held item with the list item item in front of the robot
+        
+        #self.set_light_on    :   turn on robot's light. Otherwise, false. | Stretch: triggers lights when movement is left or right
+        #self.set_light_off   :   non-movement
+
+    
+     
+    
 
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
